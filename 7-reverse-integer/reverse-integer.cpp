f73bea090f2f24dec625162bd6f7307b22702169
arijit-bit <arijitpatra204@gmail.com>
2025-07-31 01:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (53.86%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int rev = 0;
+        while(x != 0){
+            int dig = x%10;
+            rev = rev*10 + dig;
+            x /= 10;
+            if (rev > INT32_MAX || rev < INT32_MIN) {
+                return 0;
+            }
+        }
+        return rev;
+    }
+
+};
