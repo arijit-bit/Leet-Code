73bac21aefaf11da4f5da02acf1d58be1150cebc
Arijit Patra <arijitpatra204@gmail.com>
2026-07-14 03:02:26 +0530

    Time: 30 ms (88.66%) | Memory: 60.5 MB (5.89%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums); // Sort first
+        Arrays.sort(nums);
         int n = nums.length;
+        for(int i=0; i<n-2; i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int curr = nums[i];
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                
+                int sum = nums[left] + nums[right] + curr;
+                if(sum == 0){
+                    List<Integer> temp = new ArrayList();
+                    temp.add(curr);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    ans.add(temp);
+                    while(left<right && nums[left]==nums[left+1])left++;
+                    while(left<right && nums[right]==nums[right-1])right--;
 
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate 'i'
-
-            int left = i + 1, right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // Move left & right pointers to skip duplicates
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
+                }
+                if(sum<0){
                     left++;
+                }else{
                     right--;
-                } else if (sum < 0) {
-                    left++; // Need bigger sum
-                } else {
-                    right--; // Need smaller sum
                 }
             }
         }
