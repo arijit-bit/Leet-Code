7e17ce2338f26e78cd6aeae56cab508d85c6acc5
Arijit Patra <arijitpatra204@gmail.com>
2026-08-14 18:00:51 +0530

    Time: 3 ms (3.65%) | Memory: 43 MB (26.13%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        Stack <Character> st = new Stack<>();
-        if(s.length() == 0) return true;
-        for(int i=s.length()-1; i>=0; i--){
-            st.push(s.charAt(i));
-        }
-        for(int i = 0; i<t.length(); i++){
-            char curr = t.charAt(i);
-            if(!st.isEmpty() && st.peek() == curr){
-                st.pop();
+        int left = 0;
+        int right = 0;
+            if(s.length() == 0) return true;
+            while(right< t.length() && left <  s.length()){
+            char curr = s.charAt(left);
+                if(curr == t.charAt(right)){
+                    left = left+1;
+                    System.out.println(left);
+                }
+                right++;
             }
-        }
-    return st.isEmpty();
-
+        return left == s.length();
     }
 }
