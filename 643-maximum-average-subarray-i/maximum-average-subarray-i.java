71265d52c2dfe27a0666635eef4500afb98036e4
Arijit Patra <arijitpatra204@gmail.com>
2026-09-09 05:14:33 +0530

    Time: 6 ms (11.88%) | Memory: 69 MB (96.35%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -5,16 +5,31 @@ class Solution {
         double avg = 0.00000;
         double ans = Integer.MIN_VALUE;
 
-        for(int i=0; i<k; i++){
+        // for(int i=0; i<k; i++){
+        //     sum += nums[i];
+        // }
+        // avg = sum/k;
+        // ans = Math.max(ans, avg);
+        // for(int i=k; i<n; i++){
+        //     sum += nums[i];
+        //     sum -= nums[i-k];
+        //     avg = sum/k;
+        //     ans = Math.max(ans, avg);
+        // }
+
+
+        for(int i=0; i<n; i++){
             sum += nums[i];
-        }
-        avg = sum/k;
-        ans = Math.max(ans, avg);
-        for(int i=k; i<n; i++){
-            sum += nums[i];
-            sum -= nums[i-k];
-            avg = sum/k;
-            ans = Math.max(ans, avg);
+            
+            
+            if(i>=k){
+                sum -= nums[i-k];
+            }
+            if(i>=k-1){
+                avg = sum/k;
+                ans = Math.max(ans, avg);
+            }
+            
         }
         return ans;
     }
