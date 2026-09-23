a759478649039243db66e8bff21d78209e3c001e
Arijit Patra <arijitpatra204@gmail.com>
2026-09-23 20:05:31 +0530

    Time: 458 ms (78.25%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,9 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    set N = N-1;
-  RETURN (  # Write your MySQL query statement below.
-    select distinct salary from Employee
+set n = N-1;
+  RETURN (
+    select distinct salary from Employee 
     order by salary desc
-    limit 1 offset N
+    limit 1 offset n
+    
+
   );
 END
